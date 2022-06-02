4cb72fb3f4de06eb4ffe82307671ed57d7a970ae
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2022-06-02 17:15:09 -0400

    added sneaker project icon and updated resume to redacted

--- src/styles/frontpagestyles.js
+++ src/styles/frontpagestyles.js
@@ -111,7 +111,7 @@ export function ContentProjects(props) {
       <a href="https://benjaminhuang13.github.io/chess-app/">
         <ContentProjImg src={props.imgsrc1} alt="React Chess App" />
       </a>
-      <a href="https://github.com/benjaminhuang13/SNKRScraper-GoogleCal/blob/main/google_cal_event.png">
+      <a href="https://github.com/benjaminhuang13/SNKRScraper-GoogleCal">
         <ContentProjImg src={props.imgsrc2} alt="SNKR Scraper" />
       </a>
     </ProjectContentCentered>
